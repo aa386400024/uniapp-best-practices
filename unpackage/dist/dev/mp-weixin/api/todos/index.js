0df1da4785b1de205b983c07fa161fa1b591784e
zcl <41391477@qq.com>
2023-06-30 19:06:38 +0800

    id

--- unpackage/dist/dev/mp-weixin/api/todos/index.js
+++ unpackage/dist/dev/mp-weixin/api/todos/index.js
@@ -1,1 +1,1 @@
-"use strict";const t=require("../../utils/request.js");async function e(){const s={url:"/facts/random",method:"GET"};return(await t.request(s)).data}exports.fetchTodos=e;
+"use strict";const t=require("../../utils/api/request.js");async function e(){const s={url:"/facts/random",method:"GET"};return(await t.request(s)).data}exports.fetchTodos=e;
